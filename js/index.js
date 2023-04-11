25963038329c9bf15b7d9678c75ff26d875dccff
xG10x <galindo86@hotmail.com>
2023-04-11 18:36:13 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
-// Iteration 1: Names and Input
 
+let hacker1 = "Jose";
+console.log("the driver is", hacker1);
 
-// Iteration 2: Conditionals
+let hacker2 = "Pepe";
+console.log("the navigator name is ", hacker2);
 
+if (hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has, ${hacker1.length}, characters.`);
+    
+   } else if (hacker2.length > hacker1.length){
+    console.log("It seems that the navigator has the longest name, it has", `${hacker2.length} characters.`);
+   }
 
-// Iteration 3: Loops
+   else {
+      console.log(`Wow, you both have equally long names, , ${hacker1.length}, characters`);
+   }
+  
+
+   let newHacker = "";
+   for (let char of hacker1){
+    newHacker += char.toUpperCase() + " ";
+   }
+console.log(newHacker);
